2f70f4fdf834acc38fcd4077ebf3fec801575de6
Suzuki64 <hiden.suzuki@gmail.com>
2022-04-16 14:55:44 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const menu = document.getElementById("menu");
+menu.addEventListener('click', () => {
+  menu.classList.toggle('open');
+  const nav = document.getElementById("nav");
+  nav.classList.toggle('in');
+});
+
